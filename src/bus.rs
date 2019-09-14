b44a8d8b9adc8727c47f479498be04b61ca93f8b
Max Carvajal <jcarvajal@crimson.ua.edu>
2019-09-13 21:49:38 -0500

    Hooked up cpu and bus to each other

--- src/bus.rs
+++ src/bus.rs
@@ -1,18 +1,24 @@
-use super::olc6502;
+//use super::olc6502;
 
 pub struct Bus {
-    cpu: olc6502::Olc6502,
+    //cpu: olc6502::Olc6502,
     ram: [u8; 64 * 1024],
 }
 
 impl Bus {
-    fn write(&self, addr: u16, data: u8) {
+    pub fn reset_ram(mut self) {
+        for i in 0..self.ram.len() {
+            self.ram[i] = 0x00;
+        }
+    }
+
+    pub fn write(mut self, addr: u16, data: u8) {
         if addr >= 0x0000 && addr <= 0xFFFF {
             self.ram[usize::from(addr)] = data;
         }
     }
 
-    fn read(&self, addr: u16) -> u8 {
+    pub fn read(&self, addr: u16) -> u8 {
         let read_only: bool = false; // this will be a parameter in the future
 
         if addr >= 0x0000 && addr <= 0xFFFF {
--- src/olc6502.rs
+++ src/olc6502.rs
@@ -1,12 +1,16 @@
 use super::bus;
 
 pub struct Olc6502 {
-
+    bus: bus::Bus
 }
 
 impl Olc6502 {
-    fn connect_bus(n: &bus::Bus) {
-        bus = n;
+    fn read(&self, addr: u16) -> u8 {
+        return self.bus.read(addr);
+    }
+
+    fn write(mut self, addr: u16, data: u8) {
+        self.bus.write(addr, data);
     }
 }
 
