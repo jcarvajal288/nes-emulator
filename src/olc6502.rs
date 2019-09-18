bda18cd813b506b9accfd7f09e68105cbb7e4958
Max Carvajal <jcarvajal@crimson.ua.edu>
2019-09-17 20:44:36 -0500

    Added ADC pos+neg=pos test

--- src/olc6502.rs
+++ src/olc6502.rs
@@ -1368,6 +1368,20 @@ mod tests {
         assert!(o.get_flag(Flags6502::C) == 0);
         assert!(o.get_flag(Flags6502::Z) == 0);
     }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn op_ADC_pos_neg_pos() {
+        let mut o: Olc6502 = create_olc6502();
+        o.accumulator = 0x78;
+        o.fetched_data = 0xEC;
+        ADC(&mut o);
+        assert!(o.accumulator == 0x64);
+        assert!(o.get_flag(Flags6502::V) == 0);
+        assert!(o.get_flag(Flags6502::N) == 0);
+        assert!(o.get_flag(Flags6502::C) == 1);
+        assert!(o.get_flag(Flags6502::Z) == 0);
+    }
     // endregion
 //endregion
 }
