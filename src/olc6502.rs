63380dbdc39950d3cca0546511d8c5ccc1e0fa3c
Max Carvajal <jcarvajal@crimson.ua.edu>
2019-09-19 20:14:28 -0500

    Added CPX and CPY instruction and tests

--- src/olc6502.rs
+++ src/olc6502.rs
@@ -564,12 +564,20 @@ fn CMP(o: &mut Olc6502) -> u8 { // Compare Memory And Accumulator
 
 #[allow(non_snake_case)]
 fn CPX(o: &mut Olc6502) -> u8 { // Compare Memory and Index X
-    return 0x0; 
+    let data = o.fetch();
+    o.set_flag(Flags6502::C, o.x_reg >= data);
+    o.set_flag(Flags6502::Z, o.x_reg == data);
+    o.set_flag(Flags6502::N, o.x_reg >= 0x80);
+    return 1; 
 }
 
 #[allow(non_snake_case)]
 fn CPY(o: &mut Olc6502) -> u8 { // Compare Memory And Index Y
-    return 0x0; 
+    let data = o.fetch();
+    o.set_flag(Flags6502::C, o.y_reg >= data);
+    o.set_flag(Flags6502::Z, o.y_reg == data);
+    o.set_flag(Flags6502::N, o.y_reg >= 0x80);
+    return 1; 
 }
 
 #[allow(non_snake_case)]
@@ -1520,6 +1528,54 @@ mod tests {
         o.accumulator = 0x70;
         o.fetched_data = 0x70;
         CMP(&mut o);
+        assert!(o.get_flag(Flags6502::C) == 1);
+        assert!(o.get_flag(Flags6502::Z) == 1);
+        assert!(o.get_flag(Flags6502::N) == 0);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn op_CPX_GT() {
+        let mut o: Olc6502 = create_olc6502();
+        o.x_reg = 0x81;
+        o.fetched_data = 0x70;
+        CPX(&mut o);
+        assert!(o.get_flag(Flags6502::C) == 1);
+        assert!(o.get_flag(Flags6502::Z) == 0);
+        assert!(o.get_flag(Flags6502::N) == 1);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn op_CPX_Zero() {
+        let mut o: Olc6502 = create_olc6502();
+        o.x_reg = 0x70;
+        o.fetched_data = 0x70;
+        CPX(&mut o);
+        assert!(o.get_flag(Flags6502::C) == 1);
+        assert!(o.get_flag(Flags6502::Z) == 1);
+        assert!(o.get_flag(Flags6502::N) == 0);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn op_CPY_GT() {
+        let mut o: Olc6502 = create_olc6502();
+        o.y_reg = 0x81;
+        o.fetched_data = 0x70;
+        CPY(&mut o);
+        assert!(o.get_flag(Flags6502::C) == 1);
+        assert!(o.get_flag(Flags6502::Z) == 0);
+        assert!(o.get_flag(Flags6502::N) == 1);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn op_CPY_Zero() {
+        let mut o: Olc6502 = create_olc6502();
+        o.y_reg = 0x70;
+        o.fetched_data = 0x70;
+        CPY(&mut o);
         assert!(o.get_flag(Flags6502::C) == 1);
         assert!(o.get_flag(Flags6502::Z) == 1);
         assert!(o.get_flag(Flags6502::N) == 0);
