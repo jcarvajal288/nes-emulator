388aac714f6126e647500b8139a874f63e4b55a3
Max Carvajal <jcarvajal@crimson.ua.edu>
2020-01-05 16:54:05 -0600

    Added test to confirm that the ppu can read from and write to the cpu bus

--- src/olc2C02.rs
+++ src/olc2C02.rs
@@ -6,11 +6,11 @@ pub struct Olc2C02 {
 }
 
 impl Olc2C02 {
-    fn cpu_read(self, addr: u16) -> u8 {
+    fn cpu_read(&self, addr: u16) -> u8 {
         return self.cpu.bus.read(addr);
     }
 
-    fn cpu_write(mut self, addr: u16, data: u8) {
+    fn cpu_write(&mut self, addr: u16, data: u8) {
         self.cpu.bus.write(addr, data);
     }
 }
@@ -20,4 +20,16 @@ pub fn create_olc2C02() -> Olc2C02 {
     return Olc2C02 {
         cpu: olc6502::create_olc6502(),
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn write_to_and_read_from_cpu_bus() {
+        let mut ppu = create_olc2C02();
+        ppu.cpu_write(0x24, 0x20);
+        assert!(ppu.cpu_read(0x24) == 0x20);
+    }
 }
