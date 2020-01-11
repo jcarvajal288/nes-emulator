047f217f6e402b02778dc02fa1b6c2f0ac876be4
Max Carvajal <jcarvajal@crimson.ua.edu>
2020-01-10 19:29:24 -0600

    Read in program and character roms into Cartridge object with tests

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -3,8 +3,14 @@ use std::fs::File;
 use std::io::Read;
 use std::convert::TryInto;
 
+const PROGRAM_ROM_CHUNK_SIZE: usize = 16384;
+const CHARACTER_ROM_CHUNK_SIZE: usize = 8192;
+
 pub struct Cartridge {
     header: Header,
+    mapper_id: u8,
+    program_rom: Vec<u8>,
+    character_rom: Vec<u8>,
 }
 
 struct Header {
@@ -27,8 +33,37 @@ pub fn create_cartridge_from_file(filename: &str) -> Option<Box<Cartridge>> {
             return None
         }
     };
+    let header = read_header(&file_buffer);
+    // the mapper id is (upper nybble of mapper2 | lower nybble of mapper1)
+    let mapper_id = ((header.mapper2 >> 4) << 4) | (header.mapper1 >> 4);
+
+    let has_trainer_block = header.mapper1 & 0x04 > 1;
+    let prg_starting_index = if has_trainer_block { 528 } else { 16 };
+
+    // read in program memory and character memory
+    let mut program_rom: Vec<u8> = Vec::new();
+    let mut character_rom: Vec<u8> = Vec::new();
+    let file_type: u8 = 1; // three types of file types.  only concerned with 1 so far
+    match file_type {
+        0 => { /* placeholder */ }
+        1 => {
+            let program_rom_len = (header.prg_rom_chunks as usize) * PROGRAM_ROM_CHUNK_SIZE;
+            let prg_ending_index = prg_starting_index + program_rom_len as usize;
+            program_rom = file_buffer[prg_starting_index..prg_ending_index].try_into().unwrap();
+
+            let character_rom_len = (header.chr_rom_chunks as usize) * CHARACTER_ROM_CHUNK_SIZE;
+            let chr_ending_index = prg_ending_index + character_rom_len as usize;
+            character_rom = file_buffer[prg_ending_index..chr_ending_index].try_into().unwrap();
+        }
+        2 => { /* placeholder */ }
+        _ => { panic!("ERROR: Unrecognized file type in rom read.")}
+    }
+
     return Some(Box::new(Cartridge {
-        header: read_header(&file_buffer)
+        header: header,
+        mapper_id: mapper_id,
+        program_rom: program_rom,
+        character_rom: character_rom,
     }))
 }
 
@@ -84,5 +119,25 @@ mod tests {
         assert!(header.tv_system1 == 0x00);
         assert!(header.tv_system2 == 0x00);
         assert!(header.unused == [0x00, 0x00, 0x00, 0x00, 0x00]);
+    }
+
+    #[test]
+    fn verify_program_rom_read() {
+        let filename = "./test_files/nestest.nes";
+        let cartridge: Box<Cartridge> = create_cartridge_from_file(filename).unwrap();
+
+        assert!(cartridge.program_rom.len() == PROGRAM_ROM_CHUNK_SIZE);
+        assert!(cartridge.program_rom.first() == Some(&0x4C));
+        assert!(cartridge.program_rom.last() == Some(&0xC5));
+    }
+
+    #[test]
+    fn verify_character_rom_read() {
+        let filename = "./test_files/nestest.nes";
+        let cartridge: Box<Cartridge> = create_cartridge_from_file(filename).unwrap();
+
+        assert!(cartridge.character_rom.len() == CHARACTER_ROM_CHUNK_SIZE);
+        assert!(cartridge.character_rom.first() == Some(&0x00));
+        assert!(cartridge.character_rom.last() == Some(&0x00));
     }
 }
