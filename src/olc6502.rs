5ccad64e42da43a5dafb56b74e2f04b774951536
Max Carvajal <jcarvajal@crimson.ua.edu>
2020-01-12 18:29:29 -0600

    Added zero and positive cases for ORA

--- src/olc6502.rs
+++ src/olc6502.rs
@@ -2246,7 +2246,7 @@ mod tests {
 
     #[test]
     #[allow(non_snake_case)]
-    fn op_ORA() {
+    fn op_ORA_negative() {
         let mut o: Olc6502 = create_olc6502();
         o.accumulator = 0xF9;
         o.fetched_data = 0x45;
@@ -2254,6 +2254,30 @@ mod tests {
         assert!(o.accumulator == 0xFD);
         assert!(o.get_flag(Flags6502::Z) == 0);
         assert!(o.get_flag(Flags6502::N) == 1);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn op_ORA_zero() {
+        let mut o: Olc6502 = create_olc6502();
+        o.accumulator = 0x00;
+        o.fetched_data = 0x00;
+        ORA(&mut o);
+        assert!(o.accumulator == 0x00);
+        assert!(o.get_flag(Flags6502::Z) == 1);
+        assert!(o.get_flag(Flags6502::N) == 0);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn op_ORA_positive() {
+        let mut o: Olc6502 = create_olc6502();
+        o.accumulator = 0x0F;
+        o.fetched_data = 0x00;
+        ORA(&mut o);
+        assert!(o.accumulator == 0x0F);
+        assert!(o.get_flag(Flags6502::Z) == 0);
+        assert!(o.get_flag(Flags6502::N) == 0);
     }
 
     #[test]
